bfeb3416f4bb315f934cc391c2c83a25cde8ec57
Oomkar Pandit <45208579+oomkarpanditstudent@users.noreply.github.com>
2019-12-28 01:19:19 +0000

    updating config

--- wdio.conf.js
+++ wdio.conf.js
@@ -62,12 +62,12 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
+        browserName: 'chrome'/*,
         'goog:chromeOptions': {
         // to run chrome headless the following flags are required
         // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
          args: ['--headless','--no-sandbox']//, '--disable-gpu'],
-        }
+        }*/
     
     }],
     //
