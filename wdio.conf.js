16db9847a284211c1fbc9d545d62df516e831ac3
Oomkar Pandit <45208579+oomkarpanditstudent@users.noreply.github.com>
2019-12-28 18:44:10 +0000

    for docker testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
     // on a remote machine).
     runner: 'local',
-	hostname: '172.17.0.2',
+    hostname: 'localhost',
     port: 4444,
     path: '/wd/hub',
     
